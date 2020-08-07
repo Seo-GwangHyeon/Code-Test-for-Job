1d5b9f5aab45e802bc7d07522898b0cf8c28fb3c
Seo-GwangHyeon <dya2122@gmail.com>
2020-08-07 11:24:36 +0900

    단어퍼즐 정확성

--- studied/10th/word_puzzle.py
+++ studied/10th/word_puzzle.py
@@ -1,42 +1,46 @@
 from collections import deque
 def compare(a, b, index):
     if index + len(a) <= len(b):
-        for i in range(len(a)):
-            if a[i] != b[index+i]:
-                return False
-        return True
+        if b[index:index+len(a)] == a:
+            return True
+        else:
+            return False
     return False
 
-import copy as cp
 def solution(strs, t):
-    answer = 0
-    length=len(strs)
-    # bfs로 탐색
+    n = len(strs)
+    nt=len(t)
+    dp=[123456789 for _ in range(nt+1) ]
+
     bfs=deque()
     index=0
+    lenths=[]
     for s in strs:
-        # 모든 문자열이 같은지
-        # 길이가 더작은지
-        if compare(s, t, index): # 만족하면
-            result=""
-            bfs.appendleft([s, index+len(s), 1])
-    min1=len(t)+1
-    while len(bfs)>0:
+        lenths.append(len(s));
+    for i in range(n):
+        if compare(strs[i], t, index): # 만족하면
+            dp[ index+lenths[i]]=1
+            bfs.appendleft([strs[i], index+lenths[i], 1])
+    min1=nt+1
+    while bfs.__len__()>0:
+        #print(dp)
         popped=bfs.popleft()
         _str, _index, _count = popped
         if _str==t:
             min1=min(_count, min1)
-        if _count > len(t):
+        if _count > nt:
             break
-        for s in strs:
+        for i in range(n):
             # 모든 문자열이 같은지
             # 길이가 더작은지
-            if compare(s, t, _index):  # 만족하면
-                bfs.appendleft([_str+s, _index + len(s), _count+1])
-    if min1 == len(t)+1:
+            if compare(strs[i], t, _index):  # 만족하면
+                if dp[_index + lenths[i]] > _count+1:
+                    dp[_index +  lenths[i]] = _count+1
+                    bfs.appendleft([_str+strs[i], _index + lenths[i], _count+1])
+    if min1 == nt+1:
         return -1
     return min1
 
-print(solution(["a","b","c","a","b","c","a","b","c","a","b","c"], "abcdeeeeaavadsafasdfsdfvsdcacesasdcass"   ))
+print(solution(["ba","na","n","a"], "banana"))
 print(solution(["app","ap","p","l","e","ple","pp"],"apple" ))
 print(solution(["ba","an","nan","ban","n"], "banana"))
